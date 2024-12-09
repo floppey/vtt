e77f3ae4758ff08524c7d41d6489e67e0d2282ba
Johannes <johannes@grimstadgiske.no>
2024-12-09 21:22:26 +0100

    feat: initial setup of websockets using ably

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import * as Ably from "ably";
+import { AblyProvider, ChannelProvider } from "ably/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const client = new Ably.Realtime({
+    authUrl: `${process.env.NEXT_PUBLIC_HOSTNAME}/api/createTokenRequest`,
+  });
+  return (
+    <AblyProvider client={client}>
+      <ChannelProvider channelName="headlines">
+        <Component {...pageProps} />;
+      </ChannelProvider>
+    </AblyProvider>
+  );
 }
