2dd81d2faa17762401c0b1401b647ce326185f22
Johannes Grimstad Giske <floppey@users.noreply.github.com>
2024-12-10 09:35:25 +0100

    Update renderOffScreenCanvas.ts
    Try to fix error: `DOMException: CanvasRenderingContext2D.drawImage: Passed-in canvas is empty`

--- src/vtt/renderFunctions/renderOffScreenCanvas.ts
+++ src/vtt/renderFunctions/renderOffScreenCanvas.ts
@@ -1,6 +1,25 @@
 import { VTT } from "../classes/VTT";
 import { Size } from "../types/types";
 
+
+let isEmpty = true;
+
+function isCanvasEmpty(canvas: HTMLCanvasElement) {
+  if(!isEmpty) { return false; }
+  const context = canvas.getContext('2d');
+  const pixelData = context.getImageData(0, 0, canvas.width, canvas.height);
+  
+  // Check if all pixels are transparent (alpha value is 0)
+  for (let i = 3; i < pixelData.data.length; i += 4) {
+    if (pixelData.data[i] > 0) {
+      return false; // Canvas has some non-transparent content
+    }
+  }
+  
+  return true; // Canvas is empty (all pixels are transparent)
+}
+
+
 export const renderOffScreenCanvas = (vtt: VTT): void => {
   const { ctx, windowSize, offScreenCanvas } = vtt;
   const viewportSize: Size = windowSize;
@@ -24,7 +43,11 @@ export const renderOffScreenCanvas = (vtt: VTT): void => {
   const dw = viewportSize.width;
   const dh = viewportSize.height;
   try {
-    drawCtx.drawImage(offScreenCanvas, sx, sy, sw, sh, dx, dy, dw, dh);
+    if (isCanvasEmpty(offScreenCanvas)) {
+      console.warn('Source canvas is empty');
+    } else {
+      drawCtx.drawImage(offScreenCanvas, sx, sy, sw, sh, dx, dy, dw, dh);
+    }
   } catch (e) {
     console.error(e);
   }
