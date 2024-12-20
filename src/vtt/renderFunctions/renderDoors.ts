2797493d02f810f811d4ae29abd4d2c4fdbf22cf
Johannes <johannes@grimstadgiske.no>
2024-12-20 11:50:12 +0100

    Add temporary door icon

--- src/vtt/renderFunctions/renderDoors.ts
+++ src/vtt/renderFunctions/renderDoors.ts
@@ -1,7 +1,7 @@
 import { VTT } from "../classes/VTT";
 
 export const renderDoors = (vtt: VTT) => {
-  const { mapData } = vtt;
+  const { mapData, isDebug } = vtt;
   if (!mapData || (mapData.doors?.length ?? 0) === 0) {
     return;
   }
@@ -13,19 +13,34 @@ export const renderDoors = (vtt: VTT) => {
   ctx.lineWidth = Math.max(1, Math.min(4, 4 / vtt.zoom));
 
   mapData.doors.forEach((door) => {
-    if (door.isOpen) {
-      ctx.strokeStyle = "lightgreen";
-    } else if (door.isLocked) {
-      ctx.strokeStyle = "red";
-    } else if (door.blocksVision) {
-      ctx.strokeStyle = "black";
-    } else {
-      ctx.strokeStyle = "lightblue";
+    if (isDebug) {
+      if (door.isOpen) {
+        ctx.strokeStyle = "lightgreen";
+      } else if (door.isLocked) {
+        ctx.strokeStyle = "red";
+      } else if (door.blocksVision) {
+        ctx.strokeStyle = "black";
+      } else {
+        ctx.strokeStyle = "lightblue";
+      }
+      ctx.beginPath();
+      ctx.moveTo(door.start.x, door.start.y);
+      ctx.lineTo(door.end.x, door.end.y);
+      ctx.closePath();
+      ctx.stroke();
     }
+
+    // Draw door icon at the center of the door
+    const doorCenter = {
+      x: (door.start.x + door.end.x) / 2,
+      y: (door.start.y + door.end.y) / 2,
+    };
+    const iconSize = vtt.gridSize.width / 8;
+    ctx.fillStyle = "black";
+    ctx.strokeStyle = "white";
     ctx.beginPath();
-    ctx.moveTo(door.start.x, door.start.y);
-    ctx.lineTo(door.end.x, door.end.y);
-    ctx.closePath();
+    ctx.arc(doorCenter.x, doorCenter.y, iconSize, 0, Math.PI * 2);
+    ctx.fill();
     ctx.stroke();
   });
 
